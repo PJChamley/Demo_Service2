a780c0871dfd72553022e740f5bb221ac3da5ac7
Peter Chamley <peter@chamley.name>
2022-08-22 12:20:39 +0100

    Update Readme File. Update Project to use new Sub Modules.

---
+++ src/Demo.Service2.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Demo.Service2.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ src/Demo.Service2/Service2.cs
@@ -0,0 +1,14 @@
+﻿using Demo.Common;
+
+namespace Demo.Service2
+{
+    public class Service2 : IService
+    {
+        public string ValuePassedIn { get; set; }
+
+        public string DoSomeWork()
+        {
+            return $"Value: ${ValuePassedIn} was found in {Environment.NewLine}Property:'ValuePassedIn' ";
+        }
+    }
+}
